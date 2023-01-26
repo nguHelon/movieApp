4d9934382783093c13d1399a546b9fbedfdc65a9
Ngu Helon <nguhelon@gmail.com>
2023-01-26 21:52:21 +0100

    finished the pc view of the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/*.html'],
+  content: ['./dist/index.html'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      superDark: '#221F1F',
+      normalDark: '#232323',
+      superGray: '#2f2f2f',
+      normalGray: '#4f4f4f',
+      lightGray: '#8a8a8a',
+      superLightGray: '#fafafa',
+      superRed: '#ff0000',
+      normalRed: '#B81D24',
+      white: '#ffffff',
+      black: '#000000'
+    },
+    fontFamily: {
+      Poppins: ['Poppins', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
