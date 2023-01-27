c04d36ce3fb89f2b1c1698620911ffcbc64d561d
Ngu Helon <nguhelon@gmail.com>
2023-01-27 22:05:19 +0100

    continued the UI of the MovieApp project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/index.html'],
+  content: ['./dist/*.html'],
   theme: {
     screens: {
       sm: '480px',
@@ -10,6 +10,7 @@ module.exports = {
     },
     colors: {
       superDark: '#221F1F',
+      superDark2: '#221f1fd9',
       normalDark: '#232323',
       superGray: '#2f2f2f',
       normalGray: '#4f4f4f',
